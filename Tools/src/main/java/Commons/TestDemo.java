ab535f7876bcea4c378630716a6817eaedf7158b
wenzf <2822932334@qq.com>
2022-02-09 10:37:55 +0800

    Demo示例及优化

--- Tools/src/main/java/Commons/TestDemo.java
+++ Tools/src/main/java/Commons/TestDemo.java
@@ -1,14 +1,16 @@
 package Commons;
 
-import com.sun.xml.internal.ws.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @Author: wenzf
  * @Date 2021-8-6
- * @Description:
+ * @Description: StringUtils的使用场景
  */
 public class TestDemo {
     public static void main(String[] args) {
@@ -21,6 +23,13 @@ public class TestDemo {
         String date = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
         System.out.println(date); // 输出 2021-05-01 01:01:01
 
+        //StringUtils的字符串join连接
+        String s = StringUtils.join("a", "b");
+        System.out.println(s);
+
+        //StringUtils的字符串spilt分割
+        List<String> strings = Arrays.asList(StringUtils.split("hello,world", ","));
+        System.out.println(strings);
     }
 }
 /**
@@ -28,4 +37,6 @@ public class TestDemo {
  *
  * Wzfabc
  * 2021-10-29 10:13:27
+ * ab
+ * [hello, world]
  */
